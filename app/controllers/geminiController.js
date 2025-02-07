6f3e4fb3e94a0c6cc81c6e20fa7288251563d4a1
chimebukanian <chimebukaanyanwu@gmail.com>
2025-02-07 13:08:46 +0100

    redone

--- app/controllers/geminiController.js
+++ app/controllers/geminiController.js
@@ -14,23 +14,28 @@ GitHub: https://github.com/chimebukanian
 Lagos, Nigeria
 
 Professional Summary
-a biochemistry graduate but a driven Full Stack Software Engineer skilled in building scalable and secure software applications. Proficient in various stacks, including Node.js/Express, Django/Django REST Framework, Java Spring Boot, and C# .NET, alongside TypeScript, React, and C programming. Experienced in cloud-native development with AWS and application security as a Google-certified cybersecurity professional. Adept at delivering reliable software solutions, meeting tight deadlines, and ensuring maintainability and performance.
+Driven Full Stack Software Engineer skilled in building scalable and secure software applications. Proficient in various stacks, including Node.js/Express, FastApi, Django/Django REST Framework, Java Spring Boot, and C# .Net, TypeScript, React, etc. Experienced with the AWS platform and application security as a Google-certified cybersecurity professional. Adept at delivering reliable software solutions, and ensuring maintainability and performance.
 
 Technical Skills
 Programming Languages: Python, JavaScript, TypeScript, C, C#, C++, Java
-Frameworks:Django, Django REST Framework, Node.js, React, Java Spring Boot.
+Frameworks: Django, ASP.Net Framework, Node.js, FastApi, React, Java Spring Boot.
 Tools & Platforms: AWS, Linux, Docker, Git.
 Other Expertise: Cloud Computing, Technical Writing, CyberSecurity, Soft Skills
 
 Professional Experience
+Independent national electoral commission (INEC), Jos, plateau, Nigeria.
+ICT staff
+08/2024 to 12/2024
+the ict department provides all IT services for the organization.
 Contract Backend Developer  
 Remote – 02/2022 to 04/2023  
-Built a telemedicine platform for medical consultations, leveraging Django REST Framework for API development.
-- Collaborated with a remote team to secure a regional win in the 3MTT hackathon competition.
+contracted as a backend developer by a team in a hackathon to build a telemedicine software for virtual medical consultations for individuals with less access to medical care in remote areas, ultimately to provide access to medical services and improve lives. I wrote the APIs for the web app's backend. Basically collaborated with the remote team to secure the subregional win in the 3MTT hackathon.
 ALX Africa  (training)
 Remote – 02/2022 to 04/2023  
 Gained hands-on experience in low-level programming with C, high-level programming with Python, Flask, and JavaScript.
 Enhanced DevOps and Linux system engineering skills while contributing to collaborative projects.
+
+
 Zuri/Ingressive for Good (Backend Developer Intern)  
 Remote – 05/2022 to 07/2022 
 Developed backend solutions for Django- and Node.js-based projects.
@@ -42,7 +47,7 @@ Improved coding efficiency and problem-solving through collaborative sprints.
 Education
 Bachelor of Science in Biochemistry
 University of Ibadan (college of medicine)– 01/2019 to 05/2024
-Diploma in Computer Software Engineering  
+Diploma in Software Engineering  
 ALX Africa (Holberton School) – 02/2022 to 04/2023
 
 
@@ -56,115 +61,67 @@ https://drive.google.com/file/d/1ZeASKYju7tonGokJF1499HkwBHjozsdr/view?usp=drive
 Organizations & Fellowship
 - Member, Google Developer Student Club, University of Ibadan
 - Fellow, The Room Fellowship
-
-
-Anyanwu Chimebuka Emmanuel 
-Cybersecurity Analyst 
-Phone: +234 806 933 1070  
-Email: chimebukaayanwu@gmail.com  
-portfolio site (AI-based): https://chat-ebukai.onrender.com
-LinkedIn: http://www.linkedin.com/in/chimebuka-anyanwu
-Cybersecurity: https://www.credly.com/badges/903a3db5-8ffc-48da-965f-03d0cec39a78/public_url
-GitHub: https://github.com/chimebukanian  
- Professional Summary  
-Dedicated Cybersecurity Analyst with a strong foundation in network security, risk assessment frameworks, and vulnerability management. Certified through the Google Cybersecurity Certificate, endorsed by American council of education, Coursera and Credly.. Hands-on experience in threat analysis, SIEM tools, and intrusion detection systems (IDS).  I am seeking an entry-level cybersecurity engineer role to apply my skills in network security, security policy, and organizational risk management. Committed to identifying and mitigating cybersecurity threats while ensuring compliance with security frameworks to safeguard organizational assets.  
-Skills  
-- Authentication & Identity Access Management 
-- Cybersecurity Assessmend and documentation.
-- Information Systems and Security  
-- Intrusion Detection and prevention tools  
-- Linux  
-- Network Analyzer (sniffing tools)  
-- Network Security  
-- NIST Cybersecurity Framework  
-- Python  
-- Risk Assessment  
-- Risk Management Framework  
-- Security Information and Event Management (SIEM) tools  
-- SQL and Database
-- Threat Analysis  
-- Vulnerability Assessment  
-Work Experience
-ICT staff
-Independent national electoral commission (INEC), Jos, plateau, Nigeria.
-Contract Backend Developer  
-
-My team built a telemedicine software using Django REST Framework for API development while ensuring  application security in the software through risk assessments and mitigation strategies as well as secure coding practices. we were the 3MTT Hackathon subregional Winner.
-
-Education  Certifications & Endorsements 
-Google Cybersecurity Certificate (Coursera) 
-- Successfully completed Google Cybersecurity Certificate (8-course program on Coursera).  
-- Badge received by Credly, the largest skill verification and credentialing network.  
-- Recognized by the American Council on Education (ACE) for up to 9 college credits.  
-- Hands-on skills in networks, packet sniffers, SIEM and IDS tools, python, SQL, Bash, etc.
-B.Sc. in Biochemistry
-University of Ibadan (01/2019 – 05/2024)  
-Diploma in Software Engineering  
-ALX Africa (Holberton School) (02/2022 – 04/2023)  
-Low Level Programming With C,  Devops/systems engineering, Linux, High Level Programming with Python, Flask, JavaScript, Node, Technical Writing
-Other Certifications:  
-Python Programming Certification (Cisco Networking Academy)  
-Soft Skills Certification (Jobberman Nigeria)  
-Skills in Detail  
--Security Information and Event Management (SIEM): Hands-on experience analyzing logs and detecting anomalies.  
-- Intrusion Detection Systems (IDS): Deployed tools to monitor traffic for suspicious patterns.  
-- Risk & Vulnerability Management: Identified cybersecurity threats and applied mitigation techniques.  
-- Python for Cybersecurity: Automating security tasks and analyzing data for threat detection.  
-- Linux: Proficient in command-line tools for systems administration and network security.
 
 Anyanwu Chimebuka Emmanuel
-Phone: +234 806 933 1070  
-Contact Information:
-Phone: +234 806 933 1070  
-Email: chimebukaayanwu@gmail.com  
-portfolio site (AI-based): https://chat-ebukai.onrender.com
-LinkedIn: http://www.linkedin.com/in/chimebuka-anyanwu
-GitHub: https://github.com/chimebukanian
-Lagos, Nigeria
-Professional Summary
-I am a tech-savvy recent graduate of the University of Ibadan, with a strong background in biochemistry and scientific research and as well, computer programming and general software use e.g E-mails, Microsoft office suite, slack, zoom and others) and as well leadership and team play. I possess communication and organizational skills with the ease to multitask and prioritize effectively. I'm passionate on being an exceptional addition to the company. My career goals are to secure a role that allows me to apply my knowledge and skills to contribute to advancements and problem-solving in any organization I am in.
-Education
-Bachelor of Science in Biochemistry 
-University of Ibadan (college of medicine)– 01/2019 to 05/2024
-Diploma in Computer Software Engineering  
-ALX Africa (Holberton School) – 02/2022 to 04/2023
-Skills
-- Proficient with general computer software use e.g, GraphPad Prism, Microsoft Office Suite (Word, Excel, PowerPoint, Outlook)
-- Experience with virtual meeting/video conferencing tools (Zoom, Teams, etc.)
-- Detail-oriented with outstanding organizational skills
-- Fast on-the-job learner, able to quickly adapt to new technologies
-- Time management and ability to meet deadlines
-- Proficient in laboratory techniques and analytical methods
-- Knowledge of biochemical principles and applications
-- Computer proficiency 
-- Strong problem-solving and critical thinking abilities/wearing multiple thinking hats
-- Strong written and verbal communication abilities  
-- Attention to detail and ability to follow protocols
- 
-Certifications
-Google Cybersecurity certificate:
-https://www.credly.com/badges/903a3db5-8ffc-48da-965f-03d0cec39a78/public_url
-Soft skills certification: https://drive.google.com/file/d/1fllrroKxnHjDrG1VpuaitehdJpjCqQdM/view?usp=drivesdk 
-Software engineering: https://drive.google.com/file/d/1ZRzhQmBOUhsIw2Zx2V1q0MegwL7RLdrF/view?usp=drivesdk
-https://drive.google.com/file/d/1ZeASKYju7tonGokJF1499HkwBHjozsdr/view?usp=drivesdk
-Organizations & Fellowship
-- Member, Google Developer Student Club, University of Ibadan
-- Fellow, The Room Fellowship
-Leadership Experience
-Honorable member, Spokesman and Chairman of the Media and Publicity Committee of the Faculty Legislative Council of the Basic Medical Science Students Association,(BAMSSA FLC), University of Ibadan.
+Cybersecurity Analyst | Security Engineer 
+📞 +234 806 933 1070 | ✉️ chimebukaayanwu@gmail.com  
+🌍 Portfolio | LinkedIn | Github
+PROFESSIONAL SUMMARY
+Dedicated Cybersecurity Analyst with a strong background in network security, risk assessment, vulnerability management, and SIEM tools. Google Cybersecurity Certified, with hands-on experience in threat analysis, security auditing, and compliance frameworks (NIST, ISO 27001). Proven ability to detect, prevent, and mitigate cyber threats while ensuring business continuity. Passionate about securing digital assets and implementing industry-leading security policies.  
+CORE SKILLS
+✅ Threat Detection & Incident Response (SIEM, IDS/IPS)  
+✅ Risk Assessment & Mitigation (NIST, ISO 27001, GDPR, PCI DSS, HIPAA)  
+✅ Network & Application Security (Firewalls, VPN, Secure Coding practices)  
+✅ Vulnerability Assessment & Penetration Testing
+✅ Authentication & Identity Access Management (IAM)
+✅ Security Information and Event Management (SIEM)
+✅ Programming & Automation (Python, SQL, Bash, Linux)  
+✅ Security Compliance & Governance  
+PROFESSIONAL EXPERIENCE  
+Contract Backend Developer (Application security
+3MTT Hackathon (2022 – 2023)  
+Collaborated with a remote team to secure the subregional win of the 3MTT hackathon, where i developed secure APIs for the backend of the telemedicine web app using Django REST Framework, ensuring data protection and access control.
+Implemented authentication security (OAuth, JWT) and mitigated vulnerabilities (SQL injection, XSS, broken authentication).Also, I ensure compliance with healthcare data security standards.
+ICT Staff 
+Independent National Electoral Commission (INEC), Jos, Nigeria
+Providedtechnical support as regards IT for the organization, ensuring smooth and secure running of it's IT systems 
+EDUCATION & CERTIFICATIONS
+🎓 B.Sc. Biochemistry – **University of Ibadan (2019 – 2024)
+🎓 Diploma in Software Engineering – ALX Africa (Holberton School) (2022 – 2023) 
+Certifications:
+🏆 Google Cybersecurity Certificate – Coursera, ACE-Endorsed 🔗Credly Badge  
+🏆 Python Programming Certification – Cisco Networking Academy  
+🏆 Soft Skills Certification – Jobberman Nigeria.
+PROJECTS & TECHNICAL EXPOSURE
+🔹 Security Automation with Python –writing custom security scripts for log analysis file manipulation and incident detection.  
+🔹 Intrusion Detection/Prevention Systems (IDS)** – Hands-on experience deploying and configuring IPS like Suricata.  
+🔹 Penetration Testing – Conducted vulnerability assessments using Nmap, Metasploit, brute force tools.
+🔹 Risk Management – Assisted organizations in identifying and mitigating cyber risks using NIST and ISO frameworks.
+TOOLS & TECHNOLOGIES
+🔹 SIEM Tools: Splunk, Google chronicle
+🔹Packet Sniffers/Network Analysis: Tcpdump, Wireshark
+🔹 Penetration Testing: Kali Linux, Metasploit, Nmap  
+🔹 Programming & Scripting: Python, SQL, Bash  
+🔹 Network and Application Security  
+🔹Documentation: playbooks, OWASP 10, policies, standards, procedures,etc.
+🔹Operating Systems:Linux (Ubuntu, Kali), Windows. 
 
-fun fact: also a professional guitarist and music director, with proficiecy in vast array of genres including contempoary, afro pop, fusion, highlife, RnB, gospel, blues, rock, alternative rock, etc. also having some knowledge classial guitar. with experience in session and tracking  guitar
+
+ADDITIONAL INFORMATION
+🔹 Languages: English (Fluent)  
+🔹 Soft Skills: Leadership, Analytical Thinking, Communication  
+
 `
 exports.chatbotController = async (req, res) => {
   try {
     const { text, history } = req.body;
 
 
-      const prompt = `You are EbukAI, an AI powered web app (that's currently being interacted with) developed using the MERN stack for frontend and backend by Chimebuka, who is a software engineer (full stack) and cybersecurity analyst, and secondarily, serves as his portfolio website to highilight his professional qualifications and skills. To create a concise but encompassing elevator pitch summary about him, here are his different resume: ${chimebuka}. when asked give all details with links (even if the question was about you)
+      const prompt = `You are EbukAI, an AI powered web app built by chimebuka, and secondarily, serves as his portfolio website to highilight his professional qualifications and skills. To create a concise but encompassing elevator pitch about him, here are his different resumes: ${chimebuka}. when asked give all details with links (even if the question was about you)
 
 Meanwhile, you primarily are to answer users communicating with you.
 A user is currently conversing with you, Here is the history of a conversation between you and the user so far: ${history}, which you might need to make reference to in subsequent prompts. answer the user's next prompt.
-      User: ${text}
+     ${text}
       
     `;
 
